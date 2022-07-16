5416aa038f58cfc76e69b9925c5ef2c43b54f01e
afranzio <maniafranzio1234@gmail.com>
2022-07-16 16:22:43 +0530

    Add files via upload

--- vivekML.py
+++ vivekML.py
@@ -1,6 +1,9 @@
 from datetime import *
 import investpy
 import pandas as pd
+from ta import add_all_ta_features
+from ta.utils import dropna
+import os
 
 def five_years_before():
     five_years = (datetime.now() - timedelta(days=5*365)).strftime("%d/%m/%Y")
@@ -11,15 +14,15 @@ def get_today():
 
 def generate_stock_csv(stock, stock_country):
     df = investpy.get_stock_historical_data(stock=stock, country=stock_country, from_date=five_years_before(), to_date=get_today())
-    file_name = stock + "_" + five_years_before().replace('/', '') + "_" + get_today().replace('/', '') + '.csv'
+    file_name = stock + "" + five_years_before().replace('/', '') + "" + get_today().replace('/', '')
     print(file_name, "created successfully", stock)
-    df.to_csv(file_name + '.csv')
+    df.to_csv('stocks\\' + file_name + '.csv')
     
 def generate_index_csv(index, index_country):
     df = investpy.get_index_historical_data(index=index, country=index_country, from_date=five_years_before(), to_date=get_today())
-    file_name = index + "_" + five_years_before().replace('/', '') + "_" + get_today().replace('/', '') + '.csv'
+    file_name = index + "" + five_years_before().replace('/', '') + "" + get_today().replace('/', '')
     print(file_name, "created successfully", index)
-    df.to_csv(file_name + '.csv')
+    df.to_csv('indices\\' + file_name + '.csv')
 
 def stock_operator():
     data = pd.read_csv('stockBook.csv')
@@ -40,3 +43,16 @@ def index_operator():
         search_country = investpy.search_quotes(text=index, products=['indices'], n_results=1)
         generate_index_csv(index, search_country.country)
     
+
+def apply_ta():
+    folders = ['stocks', 'indices']
+    for folder in folders:
+        files = os.listdir(folder)
+        for file in files:
+            try:
+                df = pd.read_csv(folder + '/' + file, sep=',')
+                df = dropna(df)
+                df = add_all_ta_features(df, open="Open", high="High", low="Low", close="Close", volume="Volume", fillna=True)
+                df.to_csv(folder + '/' + file)
+            except:
+                print(folder, "ERROR", file)
